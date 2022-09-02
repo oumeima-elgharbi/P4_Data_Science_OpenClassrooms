f231e727dcd2c509f1ee0e6de076993d1d678f2d
oumeima-elgharbi <oumeima.elgharbi@gmail.com>
2022-09-02 13:53:03 +0200

    trying diff log transformation

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import pandas as pd
 import seaborn as sns
 import numpy as np
 
@@ -31,9 +32,25 @@ def display_distribution_per_feature(data_frame, all_features, nb_bins):
         plt.title('Distribution of : ' + column)
         sns.histplot(data_frame[column], bins=nb_bins)
 
+def get_features_skewness(data_frame):
+    """
+
+    :param data_frame:
+    :return:
+    """
+    df = data_frame.copy()
+    numerical_features = df.select_dtypes(include='number').columns
+    skew_features = []
+
+    for feature in numerical_features:
+        # if the absolute value is greater than 2, we will need to do a log transformation
+        if abs(df[feature].skew()) > 2:
+            skew_features.append(feature)
+
+    return skew_features
 
 # Graphs to see the effect of log transformation
-def compute_log_for_feature(data_frame, feature):
+def compute_log_effect_on_skewness(data_frame, features):
     """
 
     :param data_frame:
@@ -41,39 +58,28 @@ def compute_log_for_feature(data_frame, feature):
     :return: (list)
     """
     df = data_frame.copy()
+    all_log_transformations = pd.DataFrame({})
 
-    test = np.log(df[feature])
-    test1p = np.log1p(df[feature])
-    test2 = np.log2(df[feature])
-    test2p = np.log2(1 + df[feature])
+    for feature in features:
+        test_ln = np.log(1 + df[feature])
+        test_log2 = np.log2(1 + df[feature])
+        test_log10 = np.log10(1 + df[feature])
 
-    all_log_transformations = [test, test1p, test2, test2p]
+        all_log_transformations.append({"feature": [feature],
+                                        "ln": [abs(test_ln.skew())],
+                                        "log-2": [abs(test_log2.skew())],
+                                        "log-10": [abs(test_log10.skew())]}, ignore_index=True)
     return all_log_transformations
 
-
-# Graphs to see the effect of log transformation
-def log_distribution(all_log_transformations):
-    """
-
-    :param all_log_transformations: (list) list of dataframes
-    :return:
-    """
-    plt.title('Distribution de la variable cible après transformation log (résersible)')
-    for i, df in enumerate(all_log_transformations):
-        plt.figure(i)
-        sns.distplot(df)
-    ax = plt.gca()
-    ax.legend(['log', 'log1p', 'log2', 'log2p'])
-
-    print("coeff de skewness : si sup 2 : pas distr gaussienne !!")
-    print(" var qn : features transformées et var categ qui auront un impact sur le svar à predire")
 
 
 def log_transformation(data_frame, features):
     """
+    base = 10 then log
 
     :param data_frame:
     :param features:
+    :param base: (by default we use the natural log : ln)
     :return:
     """
     df = data_frame.copy()
@@ -82,7 +88,7 @@ def log_transformation(data_frame, features):
         # we name the new variable
         log_feature = "Log-{}".format(feature)
         # we add the transformed variable to our dataframe
-        df[log_feature] = np.log(df[feature])
+        df[log_feature] = np.log(1 + df[feature])  # we add 1 in case the feature = 0
     return df
 
 
